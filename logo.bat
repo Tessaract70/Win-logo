285bac4e5375827c8570de4dd74e1c149b69b6f1
Tessaract70 <rudedog70@gmail.com>
2016-05-18 13:36:03 -0400

    Add files via upload

---
+++ logo.bat
@@ -0,0 +1,56 @@
+@echo off
+set ESC=
+set RD=%ESC%[31m
+set GN=%ESC%[32m
+set YW=%ESC%[33m
+set BL=%ESC%[34m
+set WT=%ESC%[37m
+set RN=%ESC%[0m
+echo %RD%
+echo %RD%
+echo %RD%
+echo %RD%                                         ..:;ii111i;.                                           
+echo %RD%                                   .i11ii;;;;iii111tttffLi                                      
+echo %RD%                                 ,t111iii;;;iiii11tttfffLLLCC.                                  
+echo %RD%                                 t111iii;;;;iii111tttffLLLCCCf                                  
+echo %RD%                                ;1111iii;;;iii111tttfffLLCCCG  %GN%,1L:                             
+echo %RD%                                t111iii;;;;iii111tttffLLLCCCi  %GN%fCLLLft,,..           ....;i1tG  
+echo %RD%                               1111iii;;;;iii111tttfffLLCCCG  %GN%;fCLLfffttt111iii;;;;::;;ii111C,  
+echo %RD%                              .1111iii;;;;iii111ttfffLLLCCG,  %GN%tCLLLffttt111iii;;;:::;;;ii11fC   
+echo %RD%                              t111iii;;;;iii111tttffLLLCCCC  %GN%tLCLLfffttt111iii;;;::;;;ii111G    
+echo %RD%                             ;t111iii;;;;iii11tttfffLLLCCG  %GN%.1CLLLffttt111iii;;;:::;;;ii11Lt    
+echo %RD%                             t111iii;;;;iii111tttffLLLCCGt  %GN%fCCLLfffttt111iii;;:::;;;ii11tG     
+echo %RD%                            i1111iii;;;iii111tttfffLLCCCG  %GN%:tCLLfffttt111iii;;;::;;;iii11C;     
+echo %RD%                            t111iii;;;;iii111tttffLLLCCC;  %GN%tCLLLffttt111iii;;;:::;;;ii11tG      
+echo %RD%                           1t11ii1;;;;iii111tLLttfLLCCCG  %GN%ifCLLfffttt111iii;;:::;;;ii11tC.      
+echo %RD%                          ,tCC:'                '.LCLCG.  %GN%1CLLLffttt111iii;;;:::;;;ii11fL       
+echo %BL%                             .,,i;;;;iiii111tt;.     %RD%i;  %GN%fLCLLfffttt111iii;;;::;;;ii11tG        
+echo %BL%                         .1ftt11i;;;;iii111ttffLLCt     %GN%,tCLLfffttt111iii;;;:::;;;ii11Lt        
+echo %BL%                         fftt11i;;;;iii1111ttffLLCCGGL    %GN%:Cfffttt1111ii;;;:::;;;ii11fG         
+echo %BL%                        ,fftt11i;;;;iii111ttffLLCCGG0.  %YW%1t.   %GN%':t1111iii;;;::;1tllL''            
+echo %BL%                        fftt11i;;;;iii1111ttffLLCCG0f  %YW%itfftt:..      %GN%'''''''     %YW%.,:L.         
+echo %BL%                       ifftt11;;;;iiii111ttffLLCCGG0   %YW%iffftt111iiiiii,,....;;;ii111tf          
+echo %BL%                       fftt11i;;;;iii1111ttfLLLCGGGi  %YW%tfffttt111iiiii;;::::;;;iii111L           
+echo %BL%                      tftt11i;;;;iiii111ttffLLCCGGG  %YW%.iffftt111iiiiii;:::::;;;iii111I           
+echo %BL%                     .fftt11;;;;iiii111tttfLLCCGG0:  %YW%tfffttt111iiiii;;::::;;;iii111L            
+echo %BL%                     fftt11i;;;;iii1111ttffLLCCGGC  %YW%:1ffttt111iiiiii;::::;;;iii111f:            
+echo %BL%                    ;fftt1i;;;;iiii111ttffLLCCGG0   %YW%1ffftt111iiiiii;;::::;;iii1111L             
+echo %BL%                    fftt11;;;;iiii1111ttffLLCCGGf  %YW%1tffttt111iiiii;;::::;;;iii111L.             
+echo %BL%                   ;fft111;;;;iiiiii1ttffLLCCGGG   %YW%iffftt111iiiiii;::::;;;iii111T               
+echo %BL%                   fft111;;.         .;fLLCCGGG;  %YW%tfffttt111iiiii;;::::;;;iii111L               
+echo %BL%                  ;C;                      CCCG  %YW%,iffttt111iiiiii;::::;;;iii111f;               
+echo %YW%                                                 tfffttt111iiiii;;::::;;;iii111L                
+echo %YW%                                                  tLttt111iiiii;;::::;;;iii111f,                
+echo %YW%                                                      fLiiiiiii;::::;;;ii1tLCi                  
+echo %YW%                                                            '';::::;;''                         
+echo %WT%
+echo %WT%      oodb         dboo                                                                   ooooooooooo'
+echo %WT%       odb    8    dbo                                                                    ddddddbbbbb'
+echo %WT%        db   d8b   db                                                                     'd8     dd8'
+echo %WT%        db   d8b   db  d888888b  d8b   db  d8888b.   .d88b.   db   d8b   db  .d8888.             dd8'
+echo %WT%        88   I8I   88    `88'    888o  88  88  `8D  .8P  Y8.  88   I8I   88  88'  YP            dd8' 
+echo %WT%        88   I8I   88     88     88V8o 88  88   88  88    88  88   I8I   88  `8bo.             dd8'  
+echo %WT%        Y8   I8I   88     88     88 V8o88  88   88  88    88  Y8   I8I   88    `Y8b.          dd8'   
+echo %WT%        `8b d8'8b d8'    .88.    88  V888  88  .8D  `8b  d8'  `8b d8'8b d8'  db   8D         dd8'    
+echo %WT%         `8b8' `8d8'   Y888888P  VP   V8P  Y8888D'   `Y88P'    `8b8' `8d8'   `8888Y'        dd8'     
+echo %RN%
